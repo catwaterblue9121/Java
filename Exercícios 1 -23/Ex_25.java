159a6d68795a58c4ebdd7719454929ea2594a5d5
catwaterblue9121 <gustavo.nogueira.fukabori1a@gmail.com>
2025-04-07 11:32:42 -0300

    Add files via upload

---
+++ Exercícios 1 -23/Ex_25.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+25. Faça um programa, utilizando , que mostre na tela os números de 0 a 100.
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex_25
+{
+	public static void main(String[] args) {
+	int i = 0;
+	
+	while(i <= 100){
+	    System.out.println("O valor de i é:" + i);i++;
+	}
+	}
+}
