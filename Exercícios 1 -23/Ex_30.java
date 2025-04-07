1ef50e0e5e5d3e2638e7261f2a95148baf96a6ac
catwaterblue9121 <gustavo.nogueira.fukabori1a@gmail.com>
2025-04-07 11:52:17 -0300

    Add files via upload

---
+++ Exercícios 1 -23/Ex_30.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+30. Escreva um programa que pergunta um número ao usuário, e
+mostre sua tabuada completa desse número (de 1 até 10).
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex_30
+{
+    public static void main(String[] args) {
+   
+    Scanner ler = new Scanner(System.in);
+    int i, tabuada, resultado;
+
+    System.out.print("Qual número você quer a tabuada?  ");
+    tabuada = ler.nextInt();
+   
+    i = 0;
+
+    System.out.println("Está é a determinada tabuada:");
+    while (i < 10){
+        i++;
+        resultado = 0;
+        resultado = tabuada * i;
+       
+        System.out.println(tabuada + " X " + i + " = " + resultado);
+        }
+    }
+}
---
+++ Exercícios 1 -23/Ex_32.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+32. Faça um programa que leia 5 números e informe o maior número.
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex_32
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        int maiorNumero = Integer.MIN_VALUE;
+       
+        while(i<=5) {
+        System.out.print("Digite o " + i + " ºnúmero: ");
+        int numero = scanner.nextInt();
+       
+        if(numero > maiorNumero) {
+            maiorNumero = numero;
+           
+        }
+       
+        i++;
+       
+        }
+        System.out.println("O maior número digitado é: " + maiorNumero);
+       
+    }
+}
