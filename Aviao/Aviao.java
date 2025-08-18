5a52b8280100cbae774cb736295161422efc0623
Gustavo Nogueira <gustavo.nogueira.fukabori1a@gmail.com>
2025-08-18 11:44:41 -0300

    Add files via upload

---
+++ Aviao/Aviao.java
@@ -0,0 +1,40 @@
+
+public class Aviao {
+
+    private String tipo;
+    private String cor;
+    private double velocidade;
+    private int qtdeAssento;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getvelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getQtdeAssento() {
+        return qtdeAssento;
+    }
+
+    public void setQtdeAssento(int qtdeAssento) {
+        this.qtdeAssento = qtdeAssento;
+    }
+}
